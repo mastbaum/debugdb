mastbaum/debugdb

commit fe3f116f4fb0e3d761707d3b95bfdb7c6555584b

sort 'all tests for X' page by date. closes pennsnoplus/penn_daq #19

lib/views.js
@@ -5,7 +5,9 @@
 exports.tests_by_fec = {
     map: function(doc) {
         if(doc.config) {
-            emit(doc.config.fec_id, {'type': doc.type, 'pass': doc.pass, 'created': doc.created, 'loc': doc.config.loc});
+            var c = new Date(doc.created);
+            var created = -c.getTime();
+            emit([doc.config.fec_id, created], {'type': doc.type, 'pass': doc.pass, 'created': doc.created, 'loc': doc.config.loc});
         }
     }
 };
@@ -13,7 +15,9 @@ exports.tests_by_fec = {
 exports.tests_by_crate = {
     map: function(doc) {
         if(doc.config) {
-            emit(doc.config.crate_id, {'type': doc.type, 'pass': doc.pass, 'created': doc.created, 'loc': doc.config.loc});
+            var c = new Date(doc.created);
+            var created = -c.getTime();
+            emit([doc.config.crate_id, created], {'type': doc.type, 'pass': doc.pass, 'created': doc.created, 'loc': doc.config.loc});
         }
     }
 };
@@ -21,7 +25,9 @@ exports.tests_by_crate = {
 exports.tests_by_slot = {
     map: function(doc) {
         if(doc.config) {
-            emit({"crate": doc.config.crate_id, "slot": doc.config.slot}, {'type': doc.type, 'pass': doc.pass, 'created': doc.created, 'loc': doc.config.loc});
+            var c = new Date(doc.created);
+            var created = -c.getTime();
+            emit([{"crate": doc.config.crate_id, "slot": doc.config.slot}, created], {'type': doc.type, 'pass': doc.pass, 'created': doc.created, 'loc': doc.config.loc});
         }
     }
 };
@@ -31,7 +37,9 @@ exports.tests_by_db = {
         if(doc.config) {
             doc.config.db.forEach(
                function(db) {
-                   emit(db.db_id, {'type': doc.type, 'pass': doc.pass, 'created': doc.created, 'loc': doc.config.loc});
+                   var c = new Date(doc.created);
+                   var created = -c.getTime();
+                   emit([db.db_id, created], {'type': doc.type, 'pass': doc.pass, 'created': doc.created, 'loc': doc.config.loc});
                }
             );
         }

lib/lists.js
@@ -81,16 +81,16 @@ exports.tests = function (head, req) {
     var system_id;
     var list_template = "index.html";
     var title = "SNO+ Electronics Debugging";
-    if (req.query.key) {
+    if (req.query.startkey) {
         list_template = "test_list.html";
-        if(req.query.key[0] == "{") {
+        if(req.query.startkey[1] == "{") {
             // match a crate and slot
-            r = jQuery.parseJSON(req.query.key);
-            system_name = "Crate " + r['crate'] + ', Slot ' + r['slot'];
+            var r = jQuery.parseJSON(req.query.startkey)[0];
+            system_name = "Crate " + r.crate + ', Slot ' + r.slot;
             system_id = "";
         }
         else {
-            system_id = req.query.key;
+            system_id = jQuery.parseJSON(req.query.startkey)[0];
             if (req.path[2] == 'tests_by_fec')
                 system_name = "FEC";
             if (req.path[2] == 'tests_by_crate')

templates/config_header.html
@@ -1,14 +1,14 @@
 <h3>Configuration</h3>
 <dl>
  <dt>Location: {config.loc}</dt>
- <dt>Crate ID: <a href="{baseURL}/crate?key={config.crate_id}">{config.crate_id}</a></dt>
- <dt>Slot: <a href="{baseURL}/slot?key={&#34;crate&#34;:{config.crate_id},&#34;slot&#34;:{config.slot}}">{config.slot}</a></dt>
- <dt>FEC ID: <a href="{baseURL}/fec?key=&#34;{config.fec_id}&#34;">{config.fec_id}</a></dt>
+ <dt>Crate ID: <a href="{baseURL}/crate?startkey=[{config.crate_id}]&endkey=[{config.crate_id},{}]">{config.crate_id}</a></dt>
+ <dt>Slot: <a href="{baseURL}/slot?startkey=[{&#34;crate&#34;:{config.crate_id},&#34;slot&#34;:{config.slot}}]&endkey=[{&#34;crate&#34;:{config.crate_id},&#34;slot&#34;:{config.slot}},{}]">{config.slot}</a></dt>
+ <dt>FEC ID: <a href="{baseURL}/fec?startkey=[&#34;{config.fec_id}&#34;]&endkey=[&#34;{config.fec_id}&#34;,{}]">{config.fec_id}</a></dt>
  <dt>Daughterboards</dt>
   <dd>
    <ul>
     {#config.db}
-     <li><a href="{baseURL}/db?key=&#34;{db_id}&#34;">{db_id}</a></li>
+    <li><a href="{baseURL}/db?startkey=[&#34;{db_id}&#34;]&endkey=[&#34;{db_id}&#34;,{}]">{db_id}</a></li>
     {/config.db}
    </ul>
   </dd>

templates/listing.html
@@ -8,9 +8,9 @@
  {#rows}
   <tr>
    {?noquotes}
-     <td><a href="{baseURL}/{type}?key={id}">{id}</a></td>
+   <td><a href="{baseURL}/{type}?startkey=[{id}]&endkey=[{id},{}]">{id}</a></td>
    {:else}
-     <td><a href="{baseURL}/{type}?key=&#34;{id}&#34;">{id}</a></td>
+   <td><a href="{baseURL}/{type}?startkey=[&#34;{id}&#34;]&endkey=[&#34;{id}&#34;,{}]">{id}</a></td>
    {/noquotes}
    <td style="text-align:center">{count}</td>
  {/rows}

commit fbb325c8aa606e139802838ab5d057847f80db50

fix 'all X test' page, broken in fe3f116f4fb0e3d761707d3b95bfdb7c6555584b. @pennsnoplus/penn_daq issue 19

lib/views.js
@@ -49,7 +49,9 @@ exports.tests_by_db = {
 exports.tests_by_name = {
     map: function(doc) {
         if(doc.config) {
-            emit(doc.type, {'type': doc.type, 'pass': doc.pass, 'created': doc.created, 'config': doc.config, 'loc': doc.config.loc});
+            var c = new Date(doc.created);
+            var created = -c.getTime();
+            emit([doc.type, created], {'type': doc.type, 'pass': doc.pass, 'created': doc.created, 'config': doc.config, 'loc': doc.config.loc});
         }
     }
 };