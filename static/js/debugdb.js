8c779bdb954354110b19f21e38300e2c97c21bf7
Andy Mastbaum <amastbaum@gmail.com>
2012-07-14 13:15:17 -0400

    spring cleaning
    * move all tests headers into test_header.html
    * clean up urls. everything goes /thing/id instead of using query string startkeys and endkeys

--- static/js/debugdb.js
+++ static/js/debugdb.js
@@ -23,32 +23,3 @@ function togglePane(paneId) {
     }
 }
 
-function setCookie(elementId) {
-    var c_name = elementId;
-    var exdays = 100;
-    var exdate = new Date();
-    exdate.setDate(exdate.getDate() + exdays);
-    var value = document.getElementById(elementId).checked;
-    var c_value = escape(value) + ((exdays == null) ? "" : "; expires = " + exdate.toUTCString());
-    document.cookie = c_name + "=" + c_value;
-}
-
-function setCheckboxes() {
-    var i,x,y,ARRcookies = document.cookie.split(";");
-    for (i=0; i<ARRcookies.length; i++) {
-        x = ARRcookies[i].substr(0,ARRcookies[i].indexOf("="));
-        y = ARRcookies[i].substr(ARRcookies[i].indexOf("=") + 1);
-        if (y == "true")
-            y = true;
-        else
-            y = false;
-        x = x.replace(/^\s+|\s+$/g,"");
-        if (x == 'penn')
-            document.getElementById('penn').checked = y;
-        if (x == 'underground')
-            document.getElementById('underground').checked = y;
-        if (x == 'surface')
-            document.getElementById('surface').checked = y;
-    }
-}
-
